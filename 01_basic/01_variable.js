d109c8b2a38bc823b425c1f31c8b99d41310cb76
AkhilBisht-oops <Bishtakhil303@gmail.com>
2024-09-08 18:59:03 +0000

    datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,18 @@
+const accountID = 145367
+let accountEmailID = "bishtadr@sc.com"
+var accountPassword = "12356"
+accountCity = "Jaipur"
+let accountScope
+
+// prefer not to use var use let instead bcz of issue in bloack scope and function scope
+
+// accountID = 12444
+accountEmailID = "aerw@dispatchEvent.com"
+accountPassword = "457687"
+accountCity="sfgjhj"
+
+console.log(accountID);
+console.table([accountID,accountEmailID.accountCity,accountPassword,accountScope])
+
+
+
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict" // treat all js code as a new version
+
+// alert(3+3) we are using nodejs not browser
+
+console.log(3+3);
+
+
+// number -> 2^52
+// bigint
+// string
+// boolean -> true/false
+// Null -> standalone value
+// undefined -> not defined variable/datatype
+// object
+// symbol  -> unique
+
+console.log(typeof 3+3);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
