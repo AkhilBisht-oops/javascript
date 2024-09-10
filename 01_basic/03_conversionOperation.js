afe882628230a79bc143a7f2b71a3acde5aa4688
AkhilBisht-oops <Bishtakhil303@gmail.com>
2024-09-10 18:53:29 +0000

    operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -32,4 +32,51 @@
 
 let value = 3
 let negValue = -value
-console.log(negValue)
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " akhil"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);   => 12
+// console.log(1 + "2");   => 12
+// console.log("1" + "2");  => 12
+// console.log("1" + 2 + 2); => 122
+// console.log(1 + 2 + "2");  => 32
+
+// console.log(+true);  -> 1
+// console.log(+"");  -> 0
+// console.log(true+); -> this give error
+
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+ 3
+
+let a = num1 = num2 = num3;
+a = 2+3
+console.log(a);
+
+
+
+
+
+
+
+
+
+
+
+
