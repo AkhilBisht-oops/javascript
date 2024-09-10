9a532bdf69373f8da7f76c842b46aba3c7acdcb3
AkhilBisht-oops <Bishtakhil303@gmail.com>
2024-09-10 17:56:40 +0000

    03_conversion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = 33  -> 33
+//  let score = "33"  -> 33
+// let scope = "33abc" -> NAN = not a number
+// let scope = null  -> 0
+// let scope = undefined   -> NAN
+// let scope = true -> 1
+// let scope = false -> 0
+// let scope = "akhil"
+
+// console.log(typeof scope)
+// let valueInNumber = Number(scope)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = 1  -> true
+// let isLoggedIn = 0  -> false
+// let isLoggedIn = ""    -> false
+// let isLoggedIn = "akhil"  -> true
+
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *******************************operations*******************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
