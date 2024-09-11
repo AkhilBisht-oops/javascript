62a98b80f6d17e9a5dfab1e928cc62f50562455c
AkhilBisht-oops <Bishtakhil303@gmail.com>
2024-09-11 18:47:20 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,26 @@
+//                             this is a same datatype comparision
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//                             this is a differentr datatype comparision
+// console.log("2" > 1)   -> true
+// console.log("02" > 1)   -> true
+
+
+//                                 Avoid these type of conversion
+// console.log(null > 0);  -> false
+// console.log(null == 0);  -> false
+// console.log(null >= 0);  -> true
+
+// console.log(undefined == 0);  -> false
+// console.log(undefined > 0);  -> false
+// console.log(undefined < 0);  -> false
+
+// ===  strict check 
+// this === check comparision as well as datatype
+
+console.log("2" === 2);
+
